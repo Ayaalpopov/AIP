4c9e2c457d9551f44cc95e068f047120ba5aa94b
Ayaalpopov <73084950+Ayaalpopov@users.noreply.github.com>
2023-12-15 00:22:08 +0900

    Delete lab8 directory

--- lab8/Student.java
+++
@@ -1,8 +0,0 @@
-public class Student extends People{
-    Student(String nameP, String lastnameP, int ageP){
-        super(nameP,lastnameP,ageP);
-    }
-    void learn(){
-        System.out.println(name + " посещает занятия!");
-    }
-}
--- lab8/Teacher.java
+++
@@ -1,8 +0,0 @@
-public class Teacher extends People{
-    Teacher(String nameP, String lastnameP, int ageP){
-        super(nameP,lastnameP,ageP);
-    }
-    void teach(){
-        System.out.println(name + " ведёт урок!");
-    }
-}
